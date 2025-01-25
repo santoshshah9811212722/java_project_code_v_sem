56b21e1f67865a1e50f50b37f4dc16435ac0ad50
santosh shah <santosshah95@gmail.com>
2025-01-25 14:00:20 +0545

    pokara university question solved releated to thread

---
+++ Thread/PokharaUniversity_Thread.java
@@ -0,0 +1,18 @@
+package Thread;
+
+class DisplayMessage extends Thread {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Pokhara University");
+        }
+
+    }
+
+}
+
+public class PokharaUniversity_Thread {
+    public static void main(String[] args) {
+        DisplayMessage obj = new DisplayMessage();
+        obj.start();
+    }
+}
