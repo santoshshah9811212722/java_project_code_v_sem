87e506f99e8507d9175600ac4f673aca75f97e84
santosh shah <santosshah95@gmail.com>
2025-01-26 12:26:48 +0545

    addign ThreadPriority_Example.java

---
+++ Thread/ThreadPriority_Example.java
@@ -0,0 +1,20 @@
+package Thread;
+
+class Checkpriority extends Thread {
+    public void run() {
+        System.out.println("checking priority" + Thread.currentThread().getPriority());
+    }
+
+}
+
+public class ThreadPriority_Example {
+    public static void main(String[] args) {
+        Thread.currentThread().setPriority(6);
+        System.out.println("priority check of main class: " + Thread.currentThread().getPriority());
+
+        Checkpriority obj = new Checkpriority();
+        obj.setPriority(3);
+        obj.start();
+
+    }
+}
